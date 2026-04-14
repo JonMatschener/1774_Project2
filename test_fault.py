02cdab731918ccf43ab7856ad70efa9cb18d23d8
JonMatschener <jrm331@pitt.edu>
2026-04-14 09:21:00 -0400

    circuit change (ybus)

--- test_fault.py
+++ test_fault.py
@@ -3,22 +3,28 @@ from powerflow import PowerFlow
 
 c = Circuit("Fault Test")
 
-# Add buses
-c.addbus("Bus 1", 230)
-c.addbus("Bus 2", 230)
-c.addbus("Bus 3", 230)
+# Buses
+c.addbus("One", 15.0, "Slack", 1.0, 0.0)
+c.addbus("Two", 345.0, "PQ", 1.0, 0.0)
+c.addbus("Three", 15.0, "PV", 1.05, 0.0)
+c.addbus("Four", 345.0, "PQ", 1.0, 0.0)
+c.addbus("Five", 345.0, "PQ", 1.0, 0.0)
 
-# Set types
-c.buses["Bus 1"].bus_type = "Slack"
-c.buses["Bus 2"].bus_type = "PQ"
-c.buses["Bus 3"].bus_type = "PQ"
+# Network
+c.addtransformer("T1", "One", "Five", 0.0015, 0.02)
+c.addtransformer("T2", "Four", "Three", 0.00075, 0.01)
 
-# Add network
-c.addtransformer("T1", "Bus 1", "Bus 2", 0.01, 0.1)
-c.addtransmissionline("L1", "Bus 2", "Bus 3", 0.02, 0.25, 0, 0.04)
+c.addtransmissionline("L1", "Five", "Four", 0.00225, 0.025, 0.0, 0.44)
+c.addtransmissionline("L2", "Five", "Two", 0.0045, 0.05, 0.0, 0.88)
+c.addtransmissionline("L3", "Four", "Two", 0.009, 0.1, 0.0, 1.72)
 
-# Generator with X''
-c.addgenerator("G1", "Bus 1", 1.05, 100, xdp=0.2)
+# Generators
+c.addgenerator("G1", "One", 1.0, 0.0, xdp=0.2)
+c.addgenerator("G2", "Three", 1.05, 520.0, xdp=0.25)
+
+# Loads
+c.addload("Load2", "Two", 800.0, 280.0)
+c.addload("Load3", "Three", 80.0, 40.0)
 
 # Build Ybus
 c.calc_ybus()
@@ -26,7 +32,7 @@ c.calc_ybus()
 # Solve fault
 pf = PowerFlow()
 
-result = pf.solve(c, mode="fault", fault_bus="Bus 2")
+result = pf.solve(c, mode="fault", fault_bus="Two")
 
 print("\nFault Current:")
 print(result["Ifault"])
